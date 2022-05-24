a2ef196bc2b405d002b400a9ce91e6e915ef57ad
MarDsp <101469066+MarDsp@users.noreply.github.com>
2022-05-24 18:43:38 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -22,16 +22,18 @@ with open('model.pkl', 'rb') as f:
     mod= pickle.load(f)
 API_url_pred = "https://mardsp.pythonanywhere.com/predict"
 list_id=list(df['SK_ID_CURR'].values)
-
+var_imp=['EXT_SOURCE_1','EXT_SOURCE_2','EXT_SOURCE_3']
 
 st.title('Dashboard Scoring Credit')
-st.subheader("Prédictions de scoring client")
+st.subheader("Prédictions de scoring client et comparaison à l'ensemble des clients")
 
 st.sidebar.title("Exemples d'ID client...")
 for id in list_id:
     st.sidebar.info(id)
 #pred page
-#if st.button("Veuillez saisir l\'identifiant d\'un client"):
+
+if st.checkbox('Montrer dataframe'):
+    st.dataframe(df)
 id_input = st.number_input('Veuillez saisir l\'identifiant d\'un client:',None,None,100001,1 )
 df_for_client=df.loc[df['SK_ID_CURR'] == id_input]
 target=df_for_client['TARGET'].values
@@ -39,19 +41,20 @@ df_for_client=df_for_client.drop(['TARGET','SK_ID_CURR'],axis=1)
 if int(id_input) in list_id:
     with st.spinner('Chargement du score du client...'):
         json_data = r.post(url= API_url_pred,json={'id':int(id_input)}).json()
-        st.markdown("Caractéristiques du client:")
+        st.markdown("Caractéristiques de client:")
         st.dataframe(df_for_client)
         info=json.loads(json_data)
         proba = info['output_proba']
         classe_predite = info['output_class']  
-        chaine = 'Prédiction : '  +str(classe_predite)+  ' avec **' + str(round(proba*100)) + '%** de risque de défaut (classe réelle : '+str(target) + ')'
+        chaine = 'Prédiction : '  +str(classe_predite)+  ' avec probabilité :' + str(round(proba*100)) + '%'
 
     st.markdown(chaine)
     st.subheader("Caractéristiques influençant le score")
 
-    if st.button("Détails..."):
+    if st.button("Explain Results"):
        
         with st.spinner('Chargement des détails de la prédiction...'):
+           
             def interpretabilite(dataframe,X,model):
                 little_data=dataframe.drop(['TARGET','SK_ID_CURR'],axis=1)
                 X=X.iloc[0,:]
@@ -64,6 +67,17 @@ if int(id_input) in list_id:
                 return exp
             exp=interpretabilite(df,df_for_client,mod)
             components.html(exp.as_html(), height=800)
+        #st.subheader("Les distribution des variables importantes")
+    if st.checkbox('Les distribution des variables importantes'):
+        selected_var = st.multiselect("Сhoisir des variable:", df.drop(['TARGET','SK_ID_CURR'],axis=1).columns.tolist(), default=var_imp)
+        variables=selected_var
+        for v in variables:
+            f = px.histogram(df, x=v, nbins=15, title="Distribution de "+str(v))
+            f.update_xaxes(title=str(v))
+            f.update_yaxes(title="Nombre de classes")
+            st.plotly_chart(f)
+            st.write(f"Valeur de la variable {v!r} pour le client avec un ID '{id_input!r}' est: {df_for_client.iloc[0][v]!r}")
+
 else:
     st.write("Cet ID n'est pas encore dans la base de données, veuillez entrer un autre ID.")
 
